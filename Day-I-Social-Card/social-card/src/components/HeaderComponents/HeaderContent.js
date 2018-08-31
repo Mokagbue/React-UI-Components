9227a34761363c1ce5f8a8ab6549a84d3a79d970
MichelleOk <umbralcompendium@gmail.com>
2018-08-31 17:20:41 -0600

    got things to render, still ahve a lot of work to do on this

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => {
+	return  <p>Let\'s learn React by building simple interfaces with componnets. Don\'t try to over thinkit, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React.</p>;
+};
+
+export default HeaderContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const LambdaHeader = () => {
+	return <h1>Lambda School</h1>;
+};
+
+export default LambdaHeader;
